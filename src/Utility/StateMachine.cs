ff0ecbf5db2b8eebe96303f58a551a8cb70cd9c3
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2020-08-14 19:16:54 +1000

    Added StateMachine.cs and utility folder.

---
+++ src/Utility/StateMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilFarmingGame.Utility
+{
+    /// <summary>
+    /// Finite state machine. Useful for AI and scripted events.
+    /// </summary>
+    public class StateMachine
+    {
+        private IState currentState;
+
+        public void Update()
+        {
+            currentState?.Update();
+        }
+
+        public void SetState(IState state)
+        {
+            IState prevState = currentState;
+            currentState?.OnExit(state);
+            currentState = state;
+            currentState?.OnEnter(prevState);
+        }
+    }
+
+    // TODO: Abstract class?
+    /// <summary>
+    /// Represents a state in a finite state machine.
+    /// </summary>
+    public interface IState
+    {
+        /// <summary>Called when the state is entered.</summary>
+        void OnEnter(IState previousState);
+
+        /// <summary>Called each time the state machine is updated.</summary>
+        void Update();
+
+        /// <summary>Called when the state is exited.</summary>
+        void OnExit(IState nextState);
+    }
+}
